94e31f1d4b45bc175c70c444c1e55564683489ce
Raveline <eraveline@gmail.com>
2014-05-29 16:06:36 +0200

    The basic quoter API.

--- quotproject/urls.py
+++ quotproject/urls.py
@@ -1,12 +1,28 @@
 from django.conf.urls import patterns, include, url
-
-from django.contrib import admin
-admin.autodiscover()
+from quoter import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'quotproject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'quoter.views.home', name='home'),
+    url(r'^author/new', 'quoter.views.addAuthor'),
+    url(r'^author/all', 'quoter.views.getAuthors'),
+    url(r'^author/(?P<source_id>\d+)/of/$', 'quoter.views.getAuthorsOf'),
+    url(r'^author/(?P<author_id>\d+)/update/$', 'quoter.views.updateAuthor'),
+    url(r'^author/(?P<author_id>\d+)/delete/$', 'quoter.views.deleteAuthor'),
+    url(r'^source/new', 'quoter.views.addSource'),
+    url(r'^source/all', 'quoter.views.getSources'),
+    url(r'^source/(?P<source_id>\d+)/update/$', 'quoter.views.updateSource'),
+    url(r'^source/(?P<source_id>\d+)/delete/$', 'quoter.views.deleteSource'),
+    url(r'^quote/new', 'quoter.views.addQuote'),
+    url(r'^quote/(?P<quote_id>\d+)/update/$', 'quoter.views.updateQuote'),
+    url(r'^quote/(?P<quote_id>\d+)/delete/$', 'quoter.views.deleteQuote'),
+    url(r'^find/source/(?P<source_id>\d+)$', 'quoter.views.findBySource'),
+    url(r'^find/word/(?P<word>\w+)$', 'quoter.views.findByWord'),
+    url(r'^find/author/(?P<author_id>\d+)$', 'quoter.views.findByAuthor'),
+    url(r'^find/tag/(?P<tag_id>\d+)$', 'quoter.views.findByTag'),
+    url(r'^tags/all$', 'quoter.views.poll_for_tags'),
+    url(r'^tags/find/(?P<string>\w+)$', 'quoter.views.findTagsWith'),
 )
