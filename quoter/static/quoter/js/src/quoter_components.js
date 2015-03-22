b5e73c0aff4b52117b4923b57b2107e55be2d9f7
Raveline <eraveline@gmail.com>
2015-03-22 17:02:13 +0100

    Old modules removed.

--- quoter/static/quoter/js/src/quoter_components.js
+++
@@ -1,5 +0,0 @@
-
-buildQuoter = function(folders_array, tags_array) {
-    React.render(<QuoterMenu folders={folders_array} tags={tags_array}/>, document.getElementById('quoterForms'));
-    React.render(<QuoterAccess/>, document.getElementById('main-menu-container'));
-}
