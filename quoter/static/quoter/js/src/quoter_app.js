a19564e3ac45b52080f3e2e7940e27f91a9765bd
Raveline <eraveline@gmail.com>
2015-03-22 18:12:42 +0100

    Well with user info and header had disappeared.

--- quoter/static/quoter/js/src/quoter_app.js
+++ quoter/static/quoter/js/src/quoter_app.js
@@ -6,9 +6,15 @@ var QuoterAccess = require("./tabs.js");
 var Quoter = React.createClass({
     propTypes: {
         folders: React.PropTypes.array.isRequired,
-        tags: React.PropTypes.array.isRequired
+        tags: React.PropTypes.array.isRequired,
+        userName: React.PropTypes.string.isRequired,
+        currentFolder: React.PropTypes.string.isRequired
     },
     render: function() { return (
+        <div>
+        <div className="well well-sm">
+            Logged as {this.props.userName} - Current folder : {this.props.currentFolder} - <a href="/logout">Logout</a>
+        </div>
         <div>
             <div className="col-md-2" role="navigation" id="main-menu-container">
                 <QuoterAccess/>
@@ -17,14 +23,16 @@ var Quoter = React.createClass({
                 <QuoterMenu folders={this.props.folders} tags={this.props.tags}/>
             </div>
         </div>
+        </div>
         );
     }
 });
 
 window.app = (function() {
     var app = {}
-    app.buildQuoter = function(folders_array, tags_array) {
-        React.render(<Quoter folders={folders_array} tags={tags_array}/>, document.body);
+    app.buildQuoter = function(user_name, folder_name, folders_array, tags_array) {
+        React.render(<Quoter userName={user_name} currentFolder={folder_name} folders={folders_array} 
+                      tags={tags_array}/>, document.getElementById('quoter_container'));
     }
     return app;
 });
